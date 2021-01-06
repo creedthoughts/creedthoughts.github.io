a52d4a534f911a689753db77029f5ea53c3639f4
creedthoughts <76543422+creedthoughts@users.noreply.github.com>
2021-01-05 18:51:49 -0800

    Update officegame.js

--- officegame.js
+++ officegame.js
@@ -5,6 +5,13 @@ function setup(){
     frameRate(30); //changes the framerate
 
 }
+
 function draw(){ //draw by default is 60 fps
         background(scranton);
-    }
+}
+
+function mousePressed() {
+  ellipse(mouseX, mouseY, 5, 5);
+  // prevent default
+  return false;
+}
